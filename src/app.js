96d84891204889deceac5a8bb1e7776261d4fbc0
Gargtanisha11 <guptatanisha185@gmail.com>
2024-05-06 22:23:42 +0530

    route and controller

--- src/app.js
+++ src/app.js
@@ -1,26 +1,40 @@
-import express from "express"
-import cors from "cors"
-import cookieParser  from "cookie-parser";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    crenditial:true}
-))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    crenditial: true,
+  })
+);
 
-app.use(express.json({
-    limit:"16kb",
-}))// json file in from app
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+); // json file in from app
 
-app.use(express.urlencoded({
-    extended:true,
-    limit:"16kb"
-}))// url encoder
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+); // url encoder
 
-app.use(express.static("public")) // file for put some file on server file name is public
+app.use(express.static("public")); // file for put some file on server file name is public
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 
-export {app}
+//import routers 
+
+import userRouter from "./routes/user.routes.js"
+
+
+// route declartion
+app.use("/api/v1/users",userRouter)// middleware to give control to userrouter when "users " url hits 
+
+export { app };

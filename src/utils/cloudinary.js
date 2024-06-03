c45eee07f30c701753f94ff7e3a35e7769b7866f
Gargtanisha11 <guptatanisha185@gmail.com>
2024-06-03 15:30:24 +0530

    error resolve

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,29 +1,13 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
-import { ApiError } from "./ApiError";
-import { getPublicIdFromCloudinaryUrl } from "./helperFun";
+import { getPublicIdFromCloudinaryUrl } from "./helperFun.js";
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
 });
-// this code is suppose that the file which is uplaod by user is saved locally on server we try to uplaod it on cloudinary 
-// const uploadOnCloudinary = async (localFilePath) => {
-//   try {
-//     if (!localFilePath) return null;
-//     const response = await cloudinary.uploader.upload(localFilePath, {
-//       resource_type: "auto"
-//     });
-//     fs.unlinkSync(localFilePath)
-//     return response;
-     
-    
-//   } catch (error) {
-//     fs.unlinkSync(localFilePath)
-//     return error;
-//   }
-// };
+
 const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
@@ -44,7 +28,7 @@ const deleteFromCloudinary=async(localFilePath)=>{
     if(!localFilePath) return null;
     const publicId=getPublicIdFromCloudinaryUrl(localFilePath);
     if(!publicId){
-      throw new ApiError(401,"not ifmd Public Id");
+      return null;
     }
    
 
